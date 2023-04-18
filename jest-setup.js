2f9bd603c2357b12e8181f32e40524b8eecc14c1
yatessss <897969073@qq.com>
2023-04-18 15:55:34 +0800

    feat: 添加jest运行相关配置

---
+++ jest-setup.js
@@ -0,0 +1,4 @@
+// require('react-native-reanimated/lib/commonjs/reanimated2/jestUtils').setUpTests();
+
+// eslint-disable-next-line global-require
+jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,40 @@
+const { defaults } = require('ts-jest/presets');
+
 module.exports = {
+  // ts的preset
+  ...defaults,
+  // Jest 配置基础的预设
   preset: 'react-native',
+
+  // 模块使用的文件扩展名数组
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  // 检测测试文件的模式(tests 目录下的 tsx 或 jsx 文件)
+  testRegex: '(src/components/.*\\.(test|spec))\\.[tj]sx?$',
+
+  // 匹配的路径下文件将跳过覆盖率信息
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '\\.snap$'],
+
+  // 定义文件的编译方式
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        babelConfig: true,
+        // tsconfig适配node modules下的一些文件
+        tsconfig: './tsconfig.test.json',
+      },
+    ],
+  },
+
+  // 定义了忽略进行 jest 执行的依赖包
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@testing-library|react-navigation|@react-navigation/.*|@react-native-community|react-native-reanimated)/)',
+  ],
+
+  // react-native-reanimated运行时需要一些配置
+  setupFilesAfterEnv: ['./jest-setup.js'],
+
+  // 使用工作线程进行并行化
+  workerThreads: true,
 };
