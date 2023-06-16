b0b3f86f65bd3d5051042cb140f4026ba606ae9e
yatessss <897969073@qq.com>
2023-06-16 11:13:09 +0800

    feat: 添加badge组件

---
+++ src/components/Badge/types.ts
@@ -0,0 +1,55 @@
+import { ViewStyle } from 'react-native';
+import { PropsWithChildren } from 'react';
+import { ContainerProps } from '../common';
+
+export type BadgeShape = 'circle' | 'round' | 'rectangle';
+
+export type BadgeProps = PropsWithChildren<
+  {
+    /**
+     * 徽标内容
+     */
+    content?: string;
+    /**
+     * 徽标数值
+     */
+    count?: number | string;
+    /**
+     * 徽标颜色
+     */
+    color?: string;
+    /**
+     * 文字颜色
+     */
+    fontColor?: string;
+    /**
+     * 是否为红点
+     * @default false
+     */
+    dot?: boolean;
+    /**
+     * 最大展示数字值
+     * @default 99
+     */
+    maxCount?: number;
+    /**
+     * 设置状态点的位置偏移，示例：[-10, 20]
+     * @default 徽标容器尺寸的一半
+     */
+    offset?: Array<number>;
+    /**
+     * 当数值为 0 时，是否展示徽标
+     * @default false
+     */
+    showZero?: boolean;
+    /**
+     * 形状
+     * @default circle
+     */
+    shape?: BadgeShape;
+    /**
+     * 徽标自定义style
+     */
+    badgeStyles?: ViewStyle;
+  } & ContainerProps
+>;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Base';
 export * from './Button';
 export * from './Touchable';
 export * from './Highlight';
+export * from './Badge';
